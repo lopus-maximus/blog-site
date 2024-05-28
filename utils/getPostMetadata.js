cca0b0ebbc6e313101fe34a51f4459cfe687fac2
lopus-maximus <souparnikajs2004@gmail.com>
2024-05-28 22:59:48 +0530

    changes

--- utils/getPostMetadata.js
+++ utils/getPostMetadata.js
@@ -3,12 +3,17 @@ import path from "path";
 import matter from "gray-matter";
 
 export default function getPostMetadata(basePath) {
+  const cwd = process.cwd();
+  console.log(cwd);
   const folder = basePath + "/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
   const posts = markdownPosts.map((filename) => {
-    const fileContents = fs.readFileSync(path.join(basePath, filename), "utf8");
+    const fileContents = fs.readFileSync(
+      path.join(cwd, basePath, filename),
+      "utf8"
+    );
     const matterResult = matter(fileContents);
     return {
       title: matterResult.data.title,
